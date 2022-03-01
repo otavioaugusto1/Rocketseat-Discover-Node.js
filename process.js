304c4b7ae619c9b871b5ea64e9253026af526ddb
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-01 15:20:44 -0300

    Update process.js

--- process.js
+++ process.js
@@ -2,3 +2,6 @@
 //console.log(process.argv)
 console.log("Seu nome é",process.argv[2] + " " + process.argv[3])
 
+const firstName = process.argv[2]
+const lastName = process.argv[3]
+console.log(`Seu nome é ${firstName} ${lastName}`)
